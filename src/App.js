45a3e47eb55793190a0d316ed16d7f5c307a0bb3
Alejandro Zamora <alex_speed_95@hotmail.com>
2022-12-06 13:37:04 -0700

    added single user route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
@@ -27,6 +28,7 @@ function App() {
                   }
                 />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
